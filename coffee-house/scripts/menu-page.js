e147c747c71e808c76f2f517017799a9481e53a9
Kirill <kiw197204@gmail.com>
2023-12-11 13:22:23 +0300

    fix: menu list sort + imgs links for modal

--- coffee-house/scripts/menu-page.js
+++ coffee-house/scripts/menu-page.js
@@ -76,7 +76,7 @@ dessert.forEach((x, i) => {
         [...document.querySelectorAll(`.menu__card.${e.target.closest('a').dataset.type}`)].forEach(y => {
             y.classList.toggle('active');
         });
-        [...document.querySelectorAll(`.menu__card.${e.target.closest('a').dataset.type}`)].forEach(y => {
+        [...document.querySelectorAll(`.menu__card.${e.target.closest('a').dataset.type}`)].reverse().forEach(y => {
             document.querySelector('.menu__list').prepend(y);
         });
         e.target.closest('a').classList.toggle('active');
@@ -93,7 +93,9 @@ let total = document.querySelector('.modal__total-sum');
         body.classList.toggle('block-all');
         modal.classList.toggle('active');
         let arr = eval(e.target.closest('.menu__card').dataset.type);
-        document.querySelector('.modal .img__wrapper .img').src = `./resources/${e.target.closest('.menu__card').dataset.type}-${+e.target.closest('.menu__card').dataset.id + 1}.jpg`;
+        document.querySelector('.modal .img__wrapper .img').src =
+            `./resources/${e.target.closest('.menu__card').dataset.type}-${+e.target.closest('.menu__card').dataset.id + 1}.` +
+            `${e.target.closest('.menu__card').dataset.type == 'coffee' ? 'jpg' : 'png'}`;
         document.querySelector('.modal .modal__title').innerHTML = arr[e.target.closest('.menu__card').dataset.id].name;
         document.querySelector('.modal .modal__desc').innerHTML = arr[e.target.closest('.menu__card').dataset.id].description;
         [...document.querySelectorAll('.modal__btn-wrapper .modal__btn')].forEach( x => {
