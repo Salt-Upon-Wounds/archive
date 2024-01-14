01bb813078a5b8d99101be0293688707f1850679
Salt-Upon-Wounds <kiw197204@gmail.com>
2024-01-15 00:04:38 +0300

    feat: ввод с клавиатуры (виртуальной)

--- index.js
+++ index.js
@@ -16,23 +16,28 @@ const letters = document.querySelectorAll('.letter');
 const buttons = document.querySelectorAll('.btn');
 const guesses = document.querySelector('.guesses');
 let guessesCounter = 0;
+
+function wordChecker(letter) {
+  if (wordArr.includes(letter.toLowerCase())) {
+    for (let j = 0; j < wordArr.length; j += 1) {
+      if (wordArr[j] === letter.toLowerCase())
+        letters[j].innerHTML = letter.toLowerCase();
+    }
+  } else {
+    parts[guessesCounter].classList.toggle('figure-part');
+    guessesCounter += 1;
+    guesses.innerHTML = `${guessesCounter} / 6`;
+  }
+}
+
 document.addEventListener('keydown', function (event) {
-  console.log(event.key);
+  console.log(event.code);
   if (keyboard.includes(event.key.toLowerCase())) {
     for (let i = 0; i < buttons.length; i += 1) {
       if (buttons[i].innerHTML === event.key.toLowerCase()) {
         if (!buttons[i].classList.contains('pressed')) {
           buttons[i].classList.add('pressed');
-          if (wordArr.includes(event.key.toLowerCase())) {
-            for (let i = 0; i < wordArr.length; i += 1) {
-              if (wordArr[i] === event.key.toLowerCase())
-                letters[i].innerHTML = event.key.toLowerCase();
-            }
-          } else {
-            parts[guessesCounter].classList.toggle('figure-part');
-            guessesCounter += 1;
-            guesses.innerHTML = `${guessesCounter} / 6`;
-          }
+          wordChecker(event.key);
           break;
         } else {
           break;
@@ -41,3 +46,12 @@ document.addEventListener('keydown', function (event) {
     }
   }
 });
+
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].addEventListener('click', (e) => {
+    if (!e.currentTarget.classList.contains('pressed')) {
+      e.currentTarget.classList.add('pressed');
+      wordChecker(e.currentTarget.innerHTML);
+    }
+  });
+}
