871af383af7bbc771b3a8892aa466436e420c9de
Salt-Upon-Wounds <kiw197204@gmail.com>
2024-01-16 00:24:52 +0300

    feat: refresh content of body via script instead of page reload

--- hangman/create.js
+++ hangman/create.js
@@ -1,166 +1,168 @@
-const arr = [
-  ['воздух', 'Мы им дышим'],
-  ['молоко', 'Белое, вкусное, содержит лактозу'],
-  ['рогачев', 'Где лучшая сгущенка'],
-  ['тревога', 'Что мы чувствуем перед дедлайном'],
-  [
-    'облегчение',
-    'Что чувствуем после дедлайна, когда все сделано и оценки стоят справедливо',
-  ],
-  ['человеконенавистничество', 'Синоним слову мизантропия'],
-  ['плесень', 'Растет на испорченных продуктах'],
-  ['сертификат', 'При успешном окончании курса получим получим'],
-  ['прокрастинация', 'Почему разраб не доделал нормально проект'],
-  ['костыли', 'Главный наш помощник в разработке'],
-  ['программист', 'Кто не спит ночью'],
-];
-const keyboard = 'йцукенгшщзхъфывапролджэячсмитьбю'.split('');
-const body = document.querySelector('body');
+function create() {
+  const arr = [
+    ['воздух', 'Мы им дышим'],
+    ['молоко', 'Белое, вкусное, содержит лактозу'],
+    ['рогачев', 'Где лучшая сгущенка'],
+    ['тревога', 'Что мы чувствуем перед дедлайном'],
+    [
+      'облегчение',
+      'Что чувствуем после дедлайна, когда все сделано и оценки стоят справедливо',
+    ],
+    ['человеконенавистничество', 'Синоним слову мизантропия'],
+    ['плесень', 'Растет на испорченных продуктах'],
+    ['сертификат', 'При успешном окончании курса получим получим'],
+    ['прокрастинация', 'Почему разраб не доделал нормально проект'],
+    ['костыли', 'Главный наш помощник в разработке'],
+    ['программист', 'Кто не спит ночью'],
+  ];
+  const keyboard = 'йцукенгшщзхъфывапролджэячсмитьбю'.split('');
+  const body = document.querySelector('body');
 
-const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-svg.classList.add('figure-container');
-// Rod
-let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-line.setAttribute('x1', 60);
-line.setAttribute('y1', 20);
-line.setAttribute('x2', 140);
-line.setAttribute('y2', 20);
-svg.append(line);
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.classList.add('figure-container');
+  // Rod
+  let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  line.setAttribute('x1', 60);
+  line.setAttribute('y1', 20);
+  line.setAttribute('x2', 140);
+  line.setAttribute('y2', 20);
+  svg.append(line);
 
-line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-line.setAttribute('x1', 140);
-line.setAttribute('y1', 20);
-line.setAttribute('x2', 140);
-line.setAttribute('y2', 50);
-svg.append(line);
+  line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  line.setAttribute('x1', 140);
+  line.setAttribute('y1', 20);
+  line.setAttribute('x2', 140);
+  line.setAttribute('y2', 50);
+  svg.append(line);
 
-line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-line.setAttribute('x1', 60);
-line.setAttribute('y1', 20);
-line.setAttribute('x2', 60);
-line.setAttribute('y2', 230);
-svg.append(line);
+  line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  line.setAttribute('x1', 60);
+  line.setAttribute('y1', 20);
+  line.setAttribute('x2', 60);
+  line.setAttribute('y2', 230);
+  svg.append(line);
 
-line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-line.setAttribute('x1', 20);
-line.setAttribute('y1', 230);
-line.setAttribute('x2', 100);
-line.setAttribute('y2', 230);
-svg.append(line);
+  line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  line.setAttribute('x1', 20);
+  line.setAttribute('y1', 230);
+  line.setAttribute('x2', 100);
+  line.setAttribute('y2', 230);
+  svg.append(line);
 
-// body
-line = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-line.setAttribute('cx', 140);
-line.setAttribute('cy', 70);
-line.setAttribute('r', 20);
-line.classList.add('figure-part');
-svg.append(line);
+  // body
+  line = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+  line.setAttribute('cx', 140);
+  line.setAttribute('cy', 70);
+  line.setAttribute('r', 20);
+  line.classList.add('figure-part');
+  svg.append(line);
 
-line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-line.setAttribute('x1', 140);
-line.setAttribute('y1', 90);
-line.setAttribute('x2', 140);
-line.setAttribute('y2', 150);
-line.classList.add('figure-part');
-svg.append(line);
+  line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  line.setAttribute('x1', 140);
+  line.setAttribute('y1', 90);
+  line.setAttribute('x2', 140);
+  line.setAttribute('y2', 150);
+  line.classList.add('figure-part');
+  svg.append(line);
 
-line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-line.setAttribute('x1', 140);
-line.setAttribute('y1', 120);
-line.setAttribute('x2', 160);
-line.setAttribute('y2', 100);
-line.classList.add('figure-part');
-svg.append(line);
+  line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  line.setAttribute('x1', 140);
+  line.setAttribute('y1', 120);
+  line.setAttribute('x2', 160);
+  line.setAttribute('y2', 100);
+  line.classList.add('figure-part');
+  svg.append(line);
 
-line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-line.setAttribute('x1', 140);
-line.setAttribute('y1', 120);
-line.setAttribute('x2', 120);
-line.setAttribute('y2', 100);
-line.classList.add('figure-part');
-svg.append(line);
+  line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  line.setAttribute('x1', 140);
+  line.setAttribute('y1', 120);
+  line.setAttribute('x2', 120);
+  line.setAttribute('y2', 100);
+  line.classList.add('figure-part');
+  svg.append(line);
 
-line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-line.setAttribute('x1', 140);
-line.setAttribute('y1', 150);
-line.setAttribute('x2', 120);
-line.setAttribute('y2', 180);
-line.classList.add('figure-part');
-svg.append(line);
+  line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  line.setAttribute('x1', 140);
+  line.setAttribute('y1', 150);
+  line.setAttribute('x2', 120);
+  line.setAttribute('y2', 180);
+  line.classList.add('figure-part');
+  svg.append(line);
 
-line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-line.setAttribute('x1', 140);
-line.setAttribute('y1', 150);
-line.setAttribute('x2', 160);
-line.setAttribute('y2', 180);
-line.classList.add('figure-part');
-svg.append(line);
+  line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  line.setAttribute('x1', 140);
+  line.setAttribute('y1', 150);
+  line.setAttribute('x2', 160);
+  line.setAttribute('y2', 180);
+  line.classList.add('figure-part');
+  svg.append(line);
 
-const word = arr[Math.floor(Math.random() * arr.length)];
+  const word = arr[Math.floor(Math.random() * arr.length)];
 
-const panel = document.createElement('div');
-panel.className = 'playground';
-const wordDiv = document.createElement('div');
-wordDiv.className = 'word';
-// eslint-disable-next-line no-restricted-syntax, guard-for-in, no-unused-vars
-for (const x in word[0].split('')) {
-  const letter = document.createElement('span');
-  letter.className = 'letter';
-  letter.innerHTML = '_';
-  wordDiv.append(letter);
+  const panel = document.createElement('div');
+  panel.className = 'playground';
+  const wordDiv = document.createElement('div');
+  wordDiv.className = 'word';
+  // eslint-disable-next-line no-restricted-syntax, guard-for-in, no-unused-vars
+  for (const x in word[0].split('')) {
+    const letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = '_';
+    wordDiv.append(letter);
+  }
+  const hint = document.createElement('p');
+  hint.className = 'hint';
+  hint.innerHTML = `Подсказка: <span class="hint-placer">${word[1]}</span>`;
+  const counter = document.createElement('p');
+  counter.className = 'counter';
+  counter.innerHTML = 'Попытки: <span class="guesses">0 / 6</span>';
+  const keys = document.createElement('div');
+  keys.className = 'keyboard';
+  let keysRow = document.createElement('div');
+  keysRow.className = 'keyboard__row';
+  let i = 0;
+  for (; i < 12; i += 1) {
+    const btn = document.createElement('button');
+    btn.className = 'btn';
+    btn.innerHTML = `${keyboard[i]}`;
+    keysRow.append(btn);
+  }
+  keys.append(keysRow);
+  keysRow = document.createElement('div');
+  keysRow.className = 'keyboard__row';
+  for (; i - 12 < 11; i += 1) {
+    const btn = document.createElement('button');
+    btn.className = 'btn';
+    btn.innerHTML = `${keyboard[i]}`;
+    keysRow.append(btn);
+  }
+  keys.append(keysRow);
+  keysRow = document.createElement('div');
+  keysRow.className = 'keyboard__row';
+  for (; i - 12 - 11 < 9; i += 1) {
+    const btn = document.createElement('button');
+    btn.className = 'btn';
+    btn.innerHTML = `${keyboard[i]}`;
+    keysRow.append(btn);
+  }
+  keys.append(keysRow);
+
+  const back = document.createElement('div');
+  back.className = 'background hide';
+  const modal = document.createElement('div');
+  modal.className = 'modal hide';
+  const modalText = document.createElement('p');
+  modalText.className = 'modal-text';
+  const modalBtn = document.createElement('button');
+  modalBtn.className = 'modal-btn';
+  modalBtn.innerHTML = 'Перезагрузить';
+
+  modal.append(modalText, modalBtn);
+
+  panel.append(wordDiv, hint, counter, keys);
+  body.prepend(back, modal, svg, panel);
+
+  return word;
 }
-const hint = document.createElement('p');
-hint.className = 'hint';
-hint.innerHTML = `Подсказка: <span class="hint-placer">${word[1]}</span>`;
-const counter = document.createElement('p');
-counter.className = 'counter';
-counter.innerHTML = 'Попытки: <span class="guesses">0 / 6</span>';
-const keys = document.createElement('div');
-keys.className = 'keyboard';
-let keysRow = document.createElement('div');
-keysRow.className = 'keyboard__row';
-let i = 0;
-for (; i < 12; i += 1) {
-  const btn = document.createElement('button');
-  btn.className = 'btn';
-  btn.innerHTML = `${keyboard[i]}`;
-  keysRow.append(btn);
-}
-keys.append(keysRow);
-keysRow = document.createElement('div');
-keysRow.className = 'keyboard__row';
-for (; i - 12 < 11; i += 1) {
-  const btn = document.createElement('button');
-  btn.className = 'btn';
-  btn.innerHTML = `${keyboard[i]}`;
-  keysRow.append(btn);
-}
-keys.append(keysRow);
-keysRow = document.createElement('div');
-keysRow.className = 'keyboard__row';
-for (; i - 12 - 11 < 9; i += 1) {
-  const btn = document.createElement('button');
-  btn.className = 'btn';
-  btn.innerHTML = `${keyboard[i]}`;
-  keysRow.append(btn);
-}
-keys.append(keysRow);
 
-const back = document.createElement('div');
-back.className = 'background hide';
-const modal = document.createElement('div');
-modal.className = 'modal hide';
-const modalText = document.createElement('p');
-modalText.className = 'modal-text';
-const modalBtn = document.createElement('button');
-modalBtn.className = 'modal-btn';
-modalBtn.innerHTML = 'Перезагрузить';
-modalBtn.addEventListener('click', () => {
-  window.location.reload();
-});
-modal.append(modalText, modalBtn);
-
-panel.append(wordDiv, hint, counter, keys);
-body.prepend(back, modal, svg, panel);
-
-export default word;
+export default create;
--- hangman/index.js
+++ hangman/index.js
@@ -1,80 +1,103 @@
 // eslint-disable-next-line import/extensions
-import word from './create.js';
+import create from './create.js';
 
-console.log(word[0]);
+function init(word) {
+  const parts = document.querySelectorAll('.figure-part');
+  // 0 - head
+  // 1 - body
+  // 2, 3 - arms
+  // 4, 5 - legs
 
-const parts = document.querySelectorAll('.figure-part');
-// 0 - head
-// 1 - body
-// 2, 3 - arms
-// 4, 5 - legs
+  const keyboard = 'йцукенгшщзхъфывапролджэячсмитьбю'.split('');
+  const wordArr = word[0].split('');
 
-const keyboard = 'йцукенгшщзхъфывапролджэячсмитьбю'.split('');
-const wordArr = word[0].split('');
+  const letters = document.querySelectorAll('.letter');
+  const buttons = document.querySelectorAll('.btn');
+  const guesses = document.querySelector('.guesses');
+  const modal = document.querySelector('.modal');
+  const back = document.querySelector('.background');
+  let guessesCounter = 0;
+  let successCounter = 0;
 
-const letters = document.querySelectorAll('.letter');
-const buttons = document.querySelectorAll('.btn');
-const guesses = document.querySelector('.guesses');
-const modal = document.querySelector('.modal');
-const back = document.querySelector('.background');
-let guessesCounter = 0;
-let successCounter = 0;
-
-function wordChecker(letter) {
-  if (wordArr.includes(letter.toLowerCase())) {
-    for (let j = 0; j < wordArr.length; j += 1) {
-      if (wordArr[j] === letter.toLowerCase()) {
-        letters[j].innerHTML = letter.toLowerCase();
-        successCounter += 1;
+  function wordChecker(letter) {
+    if (wordArr.includes(letter.toLowerCase())) {
+      for (let j = 0; j < wordArr.length; j += 1) {
+        if (wordArr[j] === letter.toLowerCase()) {
+          letters[j].innerHTML = letter.toLowerCase();
+          successCounter += 1;
+        }
       }
+    } else {
+      parts[guessesCounter].classList.toggle('figure-part');
+      guessesCounter += 1;
+      guesses.innerHTML = `${guessesCounter} / 6`;
     }
-  } else {
-    parts[guessesCounter].classList.toggle('figure-part');
-    guessesCounter += 1;
-    guesses.innerHTML = `${guessesCounter} / 6`;
+
+    if (successCounter === word[0].length) {
+      modal.querySelector('.modal-text').innerHTML = 'Победа!';
+    }
+
+    if (guessesCounter === 6) {
+      modal.querySelector('.modal-text').innerHTML = 'Поражение!';
+    }
+
+    if (successCounter === word[0].length || guessesCounter === 6) {
+      modal.classList.toggle('hide');
+      back.classList.toggle('hide');
+    }
   }
 
-  if (successCounter === word[0].length) {
-    modal.querySelector('.modal-text').innerHTML = 'Победа!';
-  }
-
-  if (guessesCounter === 6) {
-    modal.querySelector('.modal-text').innerHTML = 'Поражение!';
-  }
-
-  if (successCounter === word[0].length || guessesCounter === 6) {
-    modal.classList.toggle('hide');
-    back.classList.toggle('hide');
-  }
-}
-
-function keyDownHandler(event) {
-  console.log(event.code);
-  if (
-    keyboard.includes(event.key.toLowerCase()) &&
-    !(successCounter === word[0].length || guessesCounter === 6)
-  ) {
-    for (let i = 0; i < buttons.length; i += 1) {
-      if (buttons[i].innerHTML === event.key.toLowerCase()) {
-        if (!buttons[i].classList.contains('pressed')) {
-          buttons[i].classList.add('pressed');
-          wordChecker(event.key);
-          break;
-        } else {
-          break;
+  function keyDownHandler(event) {
+    console.log(event.code);
+    if (
+      keyboard.includes(event.key.toLowerCase()) &&
+      !(successCounter === word[0].length || guessesCounter === 6)
+    ) {
+      for (let i = 0; i < buttons.length; i += 1) {
+        if (buttons[i].innerHTML === event.key.toLowerCase()) {
+          if (!buttons[i].classList.contains('pressed')) {
+            buttons[i].classList.add('pressed');
+            wordChecker(event.key);
+            break;
+          } else {
+            break;
+          }
         }
       }
     }
   }
+
+  document.addEventListener('keydown', keyDownHandler);
+
+  for (let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener('click', (e) => {
+      if (!e.currentTarget.classList.contains('pressed')) {
+        e.currentTarget.classList.add('pressed');
+        wordChecker(e.currentTarget.innerHTML);
+      }
+    });
+  }
 }
 
-document.addEventListener('keydown', keyDownHandler);
+let word = create();
 
-for (let i = 0; i < buttons.length; i += 1) {
-  buttons[i].addEventListener('click', (e) => {
-    if (!e.currentTarget.classList.contains('pressed')) {
-      e.currentTarget.classList.add('pressed');
-      wordChecker(e.currentTarget.innerHTML);
+console.log(word[0]);
+
+init(word);
+
+function initBtn() {
+  document.querySelector('.modal-btn').addEventListener('click', () => {
+    const body = document.querySelector('body');
+    while (body.firstElementChild) {
+      body.firstElementChild.remove();
     }
+    word = create();
+
+    console.log(word[0]);
+
+    init(word);
+    initBtn();
   });
 }
+
+initBtn();
