1f4e20828ffc311a7941d7e35d2c28214502c606
Kirill <kiw197204@gmail.com>
2024-01-29 09:17:40 +0300

    feat: side bar

--- nonograms/index.js
+++ nonograms/index.js
@@ -1,33 +1,70 @@
-const games = [
-  {
-    name: 'Динозавр',
-    top: [[1], [3], [2], [5], [1]],
-    left: [[2], [1], [3], [3], [2, 1]],
-    square: 
-      [0,0,0,1,1,
-       0,0,0,1,0,
-       0,1,1,1,0,
-       0,1,1,1,0,
-       1,1,0,1,0],
+/* eslint-disable import/extensions */
+import games from './games.js';
+
+function createFrame(picture) {
+  const frame = document.querySelector('.frame-wrapper');
+  while (frame.firstChild) {
+    frame.removeChild(frame.firstChild);
   }
-];
+  const top = document.createElement('div');
+  top.className = 'top';
+  const left = document.createElement('div');
+  left.className = 'left';
+  const square = document.createElement('div');
+  square.className = 'square';
+  for (let i = 0; i < picture.top.length; i += 1) {
+    const tmp = document.createElement('div');
+    tmp.className = 'row';
+    for (let j = 0; j < picture.top[i].length; j += 1) {
+      const btn = document.createElement('button');
+      btn.innerHTML = picture.top[i][j];
+      tmp.append(btn);
+    }
+    top.append(tmp);
+  }
+  for (let i = 0; i < picture.left.length; i += 1) {
+    const tmp = document.createElement('div');
+    tmp.className = 'row';
+    for (let j = 0; j < picture.left[i].length; j += 1) {
+      const btn = document.createElement('button');
+      btn.innerHTML = picture.left[i][j];
+      tmp.append(btn);
+    }
+    left.append(tmp);
+  }
+  for (let i = 0; i < picture.square.length; i += 1) {
+    const btn = document.createElement('button');
+    btn.dataset.secret = picture.square[i];
+    square.append(btn);
+  }
+  const nl = document.createElement('div');
+  nl.className = 'new-line';
+  frame.append(top, nl, left, square);
+}
 
 function create() {
   const body = document.querySelector('body');
   let tmp = document.createElement('button');
   tmp.className = 'hide-btn';
-  tmp.innerHTML = '->';
+  tmp.innerHTML = '<-';
+  tmp.addEventListener('click', () => {
+    document.querySelector('.side-list-wrapper').classList.toggle('active');
+    document.querySelector('.hide-btn').classList.toggle('active');
+  });
   body.append(tmp);
   const div = document.createElement('div');
   div.className = 'side-list-wrapper';
   tmp = document.createElement('ul');
   tmp.className = 'side-list';
-  for (let i = 0; i < games.length; i++) {
+  for (let i = 0; i < games.length; i += 1) {
     const li = document.createElement('li');
     li.className = 'list-item';
     li.innerHTML = games[i].name;
     li.dataset.id = i;
     tmp.append(li);
+    li.addEventListener('click', (e) => {
+      createFrame(games[e.currentTarget.dataset.id]);
+    });
   }
   div.append(tmp);
   body.append(div);
@@ -69,50 +106,8 @@ function create() {
   body.append(tmp);
 }
 
-function createFrame(picture) {
-  const frame = document.querySelector('.frame-wrapper');
-  while (frame.firstChild) {
-    frame.removeChild(frame.firstChild);
-  }
-  const top = document.createElement('div');
-  top.className = 'top';
-  const left = document.createElement('div');
-  left.className = 'left';
-  const square = document.createElement('div');
-  square.className = 'square';
-  for (let i = 0; i < picture.top.length; i++) {
-    const tmp = document.createElement('div');
-    tmp.className = 'row';
-    for (let j = 0; j < picture.top[i].length; j++) {
-      const btn = document.createElement('button');
-      btn.innerHTML = picture.top[i][j];
-      tmp.append(btn);
-    }
-    top.append(tmp);
-  }
-  for (let i = 0; i < picture.left.length; i++) {
-    const tmp = document.createElement('div');
-    tmp.className = 'row';
-    for (let j = 0; j < picture.left[i].length; j++) {
-      const btn = document.createElement('button');
-      btn.innerHTML = picture.left[i][j];
-      tmp.append(btn);
-    }
-    left.append(tmp);
-  }
-  for (let i = 0; i < picture.square.length; i++) {
-    const btn = document.createElement('button');
-    btn.dataset.secret = picture.square[i];
-    square.append(btn);
-  }
-  const nl = document.createElement('div');
-  nl.className = 'new-line';
-  frame.append(top, nl, left, square);
-}
-
 create();
 createFrame(games[0]);
 
 const body = document.querySelector('body');
 const frame = document.querySelector('.frame-wrapper');
-
