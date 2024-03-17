e37d3529ac3d00e8f4a32fd9a452e31e27a4d728
Kirill <kiw197204@gmail.com>
2024-03-17 12:30:26 +0300

    Feat: rss pz 23 toggle background image hint (#56)
    * init: vite ts template + other configurations
    
    * Feat/rss pz 00 base classes and architecture (#39)
    
    * feat: component-based architecture with base classes to use
    
    * feat: configured eslint to work only with src folder
    
    * fix: branch regex fixed
    
    * fix: husky console spam fix
    
    * fix: eslint resolver for typescript
    
    * refactor: minor changes in base component
    
    * Feat/rss pz 01 login form (#40)
    
    * feat: page with two input fields
    
    * feat: start button and style
    
    * feat: form instead of div for login
    
    * feat: page switch after submit
    
    * Feat/rss pz 02 form validation (#42)
    
    * feat: validaion + error message
    
    * feat: colored error + text for login page
    
    * feat: localstorage used for name and surname (#43)
    
    * Feat/rss pz 04 login logout process (#44)
    
    * feat: parent for BaseComponent
    
    * feat: routing + loging/logout logic
    
    * Feat/rss pz 05 start screen (#45)
    
    * feat: greetings for logged in user instead of login page or redirection
    
    * feat: a bit more pleasant style
    
    * fix: double render for / path fixed + fixed routing problems on game_page->logout->login_page path
    
    * Feat/rss pz 08 main page base (#46)
    
    * feat: html structure for game page
    
    * feat: разбиение заданного предложения на пропорциональные кусочки
    
    * feat: puzzle pieces in the bottom container
    
    * feat: randomized order for pieces
    
    * fix: overlaping elements fix
    
    * fix: overlaping elements fix 2
    
    * fix: click event for bottom pieces
    
    * feat: click event for pieces done
    
    * Feat/rss pz 11 continue button (#47)
    
    * feat: data loading
    
    * feat: continue button
    
    * feat: continue button switch rounds and level
    
    * Feat/rss pz 12 check button (#48)
    
    * feat: check button done
    
    * feat: hightlight errors
    
    * feat: Check To Continue Btns Transition done, hightlighting right sentence done (#49)
    
    * feat: autocomplete button done (#50)
    
    * Feat/rss pz 17 translation hint (#51)
    
    * feat: translation hint done
    
    * fix: wrong round select fixed
    
    * feat: switching fully done (#52)
    
    * Feat/rss pz 19 pronunciation hint (#53)
    
    * feat: buttons without listeners
    
    * feat: hint button plays audio
    
    * feat: sound button toggles pronounce hint (#54)
    
    * Feat/rss pz 22 background image hint (#55)
    
    * feat: background for field puzzles
    
    * feat: background for bottom field puzzles
    
    * feat: toggle for background hint done

---
+++ rss-puzzle/src/app/components/base-component.ts
@@ -0,0 +1,85 @@
+export type Props<T extends HTMLElement = HTMLElement> = Partial<
+  Omit<T, 'style' | 'dataset' | 'classList' | 'children' | 'tagName'>
+> & {
+  txt?: string;
+  tag?: keyof HTMLElementTagNameMap;
+};
+
+export type ElementFnProps<T extends HTMLElement = HTMLElement> = Omit<Props<T>, 'tag'>;
+
+export class BaseComponent<T extends HTMLElement = HTMLElement> {
+  protected node: T;
+
+  public children: BaseComponent[] = [];
+
+  protected parent: BaseComponent | null = null;
+
+  constructor(props: Props<T>, ...children: (BaseComponent | HTMLElement | null)[]) {
+    this.node = document.createElement(props.tag ?? 'div') as T;
+    if (props.txt) this.node.textContent = props.txt;
+    Object.assign(this.node, props);
+    if (children) {
+      this.appendChildren(children.filter((el): el is BaseComponent | HTMLElement => el !== null));
+    }
+  }
+
+  public append(child: BaseComponent | HTMLElement): void {
+    if (child instanceof BaseComponent) {
+      this.children.push(child);
+      this.children[this.children.length - 1].parent = this;
+      this.node.append(child.getNode());
+    } else {
+      this.node.append(child);
+    }
+  }
+
+  public appendChildren(children: (BaseComponent | HTMLElement | null)[]): void {
+    children
+      .filter((el): el is BaseComponent | HTMLElement => el !== null)
+      .forEach((el) => {
+        this.append(el);
+      });
+  }
+
+  public setTextContent(text: string): void {
+    this.node.textContent = text;
+  }
+
+  public getNode() {
+    return this.node;
+  }
+
+  public addClass(className: string): void {
+    this.node.classList.add(className);
+  }
+
+  public toggleClass(className: string): void {
+    this.node.classList.toggle(className);
+  }
+
+  public removeClass(className: string): void {
+    this.node.classList.remove(className);
+  }
+
+  public containsClass(className: string): boolean {
+    return this.node.classList.contains(className);
+  }
+
+  public destroyChildren(): void {
+    this.children.reduce((_, child) => {
+      child.destroy();
+      return null;
+    }, null);
+    this.children = [];
+  }
+
+  public destroy(): void {
+    this.destroyChildren();
+    this.node.remove();
+  }
+
+  public switchPage(page: BaseComponent) {
+    this.destroyChildren();
+    this.append(page);
+  }
+}
