d52651df8ebc358f398e7254d001ec22f1cc4d9f
Kirill <kiw197204@gmail.com>
2024-03-21 17:29:55 +0300

    feat: car color

--- async-race/src/app/components/car/car.ts
+++ async-race/src/app/components/car/car.ts
@@ -3,7 +3,11 @@ import { button, div, p } from '../tags';
 import style from './styles.module.scss';
 
 export default class Car extends BaseComponent {
-  constructor(name: string) {
+  constructor(
+    name: string,
+    color?: string,
+    private car = div({ className: style.car }),
+  ) {
     super({ className: style.box });
     const selectBtn = button(style.btn, 'Select');
     const removeBtn = button(style.btn, 'Remove');
@@ -13,10 +17,13 @@ export default class Car extends BaseComponent {
     const title = p(style.title, name);
     const btnsRow = div({ className: style.buttons }, selectBtn, removeBtn, title);
     const switchBtnsRow = div({ className: style.buttons }, ABtn, BBtn);
-    const car = div({ className: style.car });
     const flag = div({ className: style.flag });
     const road = div({ className: style.road }, switchBtnsRow, car, flag);
-
+    if (color) this.changeColor(color);
     this.appendChildren([btnsRow, road]);
+  }
+
+  public changeColor(color: string) {
+    this.car.getNode().style.backgroundColor = color;
   }
 }
