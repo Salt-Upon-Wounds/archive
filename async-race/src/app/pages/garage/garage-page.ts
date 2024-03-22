36dae47b7ea9ba60383a25f55ada03448ffc04a5
Kirill <kiw197204@gmail.com>
2024-03-22 22:17:36 +0300

    feat: functions to fetch data from server

--- async-race/src/app/pages/garage/garage-page.ts
+++ async-race/src/app/pages/garage/garage-page.ts
@@ -4,35 +4,41 @@ import { button, div, input, p } from '../../components/tags';
 import style from './styles.module.scss';
 
 export default class Garage extends BaseComponent {
-  constructor() {
+  constructor(
+    private crTextInput = input(style.input, { type: 'text' }),
+    private crColorInput = input(style.colorInput, { type: 'color' }),
+    private crBtn = button(style.btn, 'Create'),
+    private upTextInput = input(style.input, { type: 'text' }),
+    private upColorInput = input(style.colorInput, { type: 'color' }),
+    private upBtn = button(style.btn, 'update'),
+    private raceBtn = button(style.btn, 'race'),
+    private resetBtn = button(style.btn, 'reset'),
+    private generateBtn = button(style.btn, 'generate cars'),
+    private title = p(style.title, `Garage (???)`),
+    private page = p(style.page, `Page (???)`),
+    private carList: Car[] = [],
+  ) {
     super({ className: style.garage });
-    const crTextInput = input(style.input, { type: 'text' });
-    const crColorInput = input(style.colorInput, { type: 'color' });
-    const crBtn = button(style.btn, 'Create');
-    const upTextInput = input(style.input, { type: 'text' });
-    const upColorInput = input(style.colorInput, { type: 'color' });
-    const upBtn = button(style.btn, 'update');
-    const raceBtn = button(style.btn, 'race');
-    raceBtn.addClass(style.green);
-    const resetBtn = button(style.btn, 'reset');
-    resetBtn.addClass(style.green);
-    const generateBtn = button(style.btn, 'generate cars');
-    generateBtn.addClass(style.wide);
+    this.raceBtn.addClass(style.green);
+    this.resetBtn.addClass(style.green);
+    this.generateBtn.addClass(style.wide);
+    // this.crBtn.getNode().onclick = this.createCar.bind(this);
 
-    const title = p(style.title, `Garage (???)`);
-    const page = p(style.page, `Page (???)`);
-    const test = [];
+    this.carList = [];
     for (let i = 0; i < 10; i += 1) {
-      test.push(new Car(`Asadasd ${i + 1}`));
+      this.carList.push(new Car(`Asadasd ${i + 1}`));
     }
 
     const wrapper = div(
       { className: style.wrapper },
-      div({ className: style.row }, crTextInput, crColorInput, crBtn),
-      div({ className: style.row }, upTextInput, upColorInput, upBtn),
-      div({ className: style.row }, raceBtn, resetBtn, generateBtn),
+      div({ className: style.row }, this.crTextInput, this.crColorInput, this.crBtn),
+      div({ className: style.row }, this.upTextInput, this.upColorInput, this.upBtn),
+      div({ className: style.row }, this.raceBtn, this.resetBtn, this.generateBtn),
     );
-    const list = div({ className: style.list }, title, page, ...test);
+    const list = div({ className: style.list }, this.title, this.page, ...this.carList);
     this.appendChildren([wrapper, list]);
   }
+
+  // name: this.crTextInput.getNode().textContent === '' ? 'DefaultName' : this.crTextInput.getNode().textContent,
+  // color: this.crColorInput.getNode().value,
 }
---
+++ async-race/src/app/utils/api.ts
@@ -0,0 +1,155 @@
+export function createCar(name: string, color: string) {
+  fetch('http://localhost:3000/garage', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, color }),
+  }).then(
+    (val) => {
+      console.log(val);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
+}
+
+// разбивает весь список машин на сервере на группы по limit машин и дает набор на page странице
+export function getCars(page: number, limit: number) {
+  fetch(`http://localhost:3000/garage?${new URLSearchParams({ _page: page.toString(), _limit: limit.toString() })}`, {
+    method: 'GET',
+  }).then(
+    (val) => {
+      console.log(val);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
+}
+
+export function getCar(id: number) {
+  fetch(`http://localhost:3000/garage?${new URLSearchParams({ id: id.toString() })}`, {
+    method: 'GET',
+  }).then(
+    (val) => {
+      console.log(val);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
+}
+
+export function deleteCar(id: number) {
+  fetch(`http://localhost:3000/garage?${new URLSearchParams({ id: id.toString() })}`, {
+    method: 'DELETE',
+  }).then(
+    (val) => {
+      console.log(val);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
+}
+
+export function updateCar(id: number, name: string, color: string) {
+  fetch(`http://localhost:3000/garage?${new URLSearchParams({ id: id.toString() })}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, color }),
+  }).then(
+    (val) => {
+      console.log(val);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
+}
+
+// перед использованием drive нужно завести машину
+export function engine(id: number, status: 'started' | 'stopped' | 'drive') {
+  fetch(`http://localhost:3000/engine?${new URLSearchParams({ id: id.toString(), status })}`, {
+    method: 'PATCH',
+  }).then(
+    (val) => {
+      console.log(val);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
+}
+
+export function getWinners(page: number, limit: number, sort: 'id' | 'wins' | 'time', order: 'ASC' | 'DESC') {
+  fetch(
+    `http://localhost:3000/winners?${new URLSearchParams({ _page: page.toString(), _limit: limit.toString(), _sort: sort, _order: order })}`,
+    {
+      method: 'GET',
+    },
+  ).then(
+    (val) => {
+      console.log(val);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
+}
+
+export function getWinner(id: number) {
+  fetch(`http://localhost:3000/winners?${new URLSearchParams({ id: id.toString() })}`, {
+    method: 'GET',
+  }).then(
+    (val) => {
+      console.log(val);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
+}
+
+export function createWinner(id: number, wins: number, time: number) {
+  fetch(`http://localhost:3000/winners`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ id, wins, time }),
+  }).then(
+    (val) => {
+      console.log(val);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
+}
+
+export function deleteWinner(id: number) {
+  fetch(`http://localhost:3000/winners?${new URLSearchParams({ id: id.toString() })}`, {
+    method: 'DELETE',
+  }).then(
+    (val) => {
+      console.log(val);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
+}
+
+export function updateWinner(id: number, wins: number, time: number) {
+  fetch(`http://localhost:3000/winners?${new URLSearchParams({ id: id.toString() })}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ wins, time }),
+  }).then(
+    (val) => {
+      console.log(val);
+    },
+    (err) => {
+      console.log(err);
+    },
+  );
+}
