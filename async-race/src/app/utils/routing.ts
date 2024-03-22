f7bf808817e23bfa8ee5e4f7fc49078d545b4fe5
Kirill <kiw197204@gmail.com>
2024-03-22 10:52:37 +0300

    feat: utility fuc for routing

---
+++ async-race/src/app/utils/routing.ts
@@ -0,0 +1,7 @@
+export default function go(path: string) {
+  window.history.pushState(
+    { path: `${path}` },
+    '',
+    `${window.location.origin}/salt-upon-wounds-JSFE2023Q4/rss-puzzle/${path}`,
+  );
+}
