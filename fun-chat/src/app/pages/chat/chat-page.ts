2e2adb5d7e3f4fcd03657f6c09051f519f220121
Kirill <kiw197204@gmail.com>
2024-04-12 13:22:59 +0300

    feat: chat layout

--- fun-chat/src/app/pages/chat/chat-page.ts
+++ fun-chat/src/app/pages/chat/chat-page.ts
@@ -1,11 +1,38 @@
 import { BaseComponent } from '../../components/base-component';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
+import { button, div, input, p } from '../../components/tags';
 import style from './styles.module.scss';
 
 export default class ChatPage extends BaseComponent {
   constructor() {
     super({ className: style.chat });
-    this.appendChildren([new Header(), new Footer()]);
+
+    const targetUser = p(style.name, 'Test');
+    const targetUserStatus = p(style.status, 'online');
+    const topRow = div({ className: style.top }, targetUser, targetUserStatus);
+
+    const messageList = div(
+      { className: style.list },
+      ...new Array(50).fill(1).map((_, idx) => button(style.elem, `test_message ${idx}`)),
+    );
+
+    const messageInput = input(style.input, { type: 'text', placeholder: 'Message' });
+    const sendBtn = button(style.send, 'Send');
+    const bottomRow = div({ className: style.bottom }, messageInput, sendBtn);
+
+    const messages = div({ className: style.messages }, topRow, messageList, bottomRow);
+
+    const userInput = input(style.search, { type: 'text', placeholder: 'Search...' });
+    const userList = div(
+      { className: style.list },
+      ...new Array(20).fill(1).map((_, idx) => button(style.elem, `test_test ${idx}`)),
+    );
+    const users = div({ className: style.users }, userInput, userList);
+    userList.children[0].addClass(style.active);
+
+    const wrapper = div({ className: style.center }, users, messages);
+
+    this.appendChildren([new Header(), wrapper, new Footer()]);
   }
 }
